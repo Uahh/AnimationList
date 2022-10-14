343783cea431a7ebaba6c3df6fc9c58a1662f27d
itorr <itorrrrrr@me.com>
2022-10-15 01:43:26 +0800

    解决超过15个输出图片一半空白的问题 fix #1

--- static_animationlist/js/result.js
+++ static_animationlist/js/result.js
@@ -7,15 +7,68 @@ const options = {
             data: {}
         };
     },
+    computed: {
+        bangumiGrunps(){
+            const { data } = this;
+            if(!data) return [];
+
+            const b = Object.values(data)
+            const c = [];
+            while(1){
+                c.push(b.splice(0,10));
+                if(!b.length) break;
+            }
+            return c;
+        }
+    },
     mounted: function () {
         this.data = eval('[' + this.$refs.data.innerText + ']')[0]
     },
     methods: {
-        createImg() {
-            html2canvas(document.querySelector("#pic")).then(canvas => {
-                var img = canvas.toDataURL()
-                document.getElementById('base64Img').setAttribute("src", img)
+        imageError(e){
+            e.target.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAAtJREFUGFdjYAACAAAFAAGq1chRAAAAAElFTkSuQmCC';
+        },
+        // createImg() {
+        //     html2canvas(document.querySelector("#pic")).then(canvas => {
+        //         var img = canvas.toDataURL()
+        //         document.getElementById('base64Img').setAttribute("src", img)
+        //     },{
+        //         scale: 2
+        //     });
+        // },
+        async createImg() {
+            const boxs = [...document.querySelectorAll('.result-head,.bangumis-group,.result-footer')];
+            const canvasList = [];
+            let width;
+            let allHeight = 0;
+
+            console.log(boxs)
+
+            for(let i = 0;boxs.length > i;i++){
+                const box = boxs[i];
+                const canvas = await html2canvas(box,{
+                    scale: 2
+                });
+                console.log(box,i,canvas);
+
+                canvasList.push(canvas);
+
+                width = canvas.width;
+                allHeight = allHeight + canvas.height;
+            }
+
+            const canvas = document.createElement('canvas');
+            canvas.width = width;
+            canvas.height = allHeight;
+            const ctx = canvas.getContext('2d');
+
+            let top = 0;
+            canvasList.forEach(canvas=>{
+                ctx.drawImage(canvas,0,top,canvas.width,canvas.height);
+                top += canvas.height;
             });
+            document.getElementById('base64Img').setAttribute('src', canvas.toDataURL());
+
         },
         confirmClick() {
             this.onDialog = true
